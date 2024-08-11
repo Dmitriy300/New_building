db83f4de5aa480893e1f5e2f3a16dce25cc074cd
Dmitriy <ddnsk54@gmail.com>
2024-08-11 12:55:50 +0300

    немного изменил код при выводе переменных

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -16,9 +16,9 @@ public class Task : MonoBehaviour
         float FloatingPointVariable = 6.34984898995003100958845893034675873722685858f;
         bool LogicalVariable = false;
 
-        Debug.LogError("Целое число-" + IntegerVariable);
-        Debug.LogError("Число с плавающей точкой-" + FloatingPointVariable);
-        Debug.LogError("Логическая переменная-" + LogicalVariable);
+        Debug.LogWarning($"Целое число {IntegerVariable}");
+        Debug.LogError($"Число с плавающей точкой {FloatingPointVariable}");
+        Debug.LogWarning($"Логическая переменная{LogicalVariable}");
     }
     
     private void Update()
