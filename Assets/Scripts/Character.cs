6332372d190b531818f141a6983bd3bad85c3344
Dmitriy <ddnsk54@gmail.com>
2024-08-15 21:33:23 +0300

    Добавил представление объекта

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    
+    public bool IsDead;
+    private void Update()
+    {
+        
+        if (IsDead) 
+        {
+            //Destroy(gameObject); //удаляет объект
+            
+        }
+        
+    }
+
+    
+
+   
+    
+}
---
+++ CharacterPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assembly_CSharp
+{
+    public class CharacterPresenter : MonoBehaviour
+    {
+        public Character Character;
+        private void Update()
+        {
+            Character.gameObject.SetActive(Character.IsDead);
+        }
+    }
+}
