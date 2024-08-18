9cb2b08315f684f9e1c6e2d720239821c7165c89
Dmitriy <ddnsk54@gmail.com>
2024-08-18 20:26:59 +0300

    Добавил метод TakeDamage

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,16 +10,32 @@ public class Character : MonoBehaviour
     private void Update()
     {
         IsDeath = Health <= 0 ? true : false;
-        if (IsDeath)
-        {
-
-        }
-
         
+        if (Health == 0)
+        {
+           gameObject.SetActive(false);
+           TakeDamage(Health);
+        }
     }
     public void SetActive(bool isActive)
     {
         gameObject.SetActive(isActive);
     }
 
+    public void TakeDamage(int damage)
+    {
+        if (IsDeath)
+            return;
+
+        Health -= damage;
+        if (Health < 0)
+        {
+            Health = 0;
+        }
+        Debug.LogError(" is dead.");
+        
+    }
+
+   
 }
+
---
+++ Assets/Scripts/CharacterSp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSp : MonoBehaviour
+{
+    public Character Character;
+    public Transform Point;
+    public int[] _ally = new int[5];
+    public int[] _enemy = new int[5];
+
+    private void Start()
+    {
+        
+    }
+}
